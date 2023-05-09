2ad560a9603930aaac78ee751e2c5bdab66e2ad1
MichaelYnoa <michaelaraujoynoa@gmail.com>
2023-05-09 19:43:51 -0400

    hooks implementation

--- __tests__/extrayendoInfo.test.js
+++ __tests__/extrayendoInfo.test.js
@@ -2,15 +2,27 @@ const puppeteer = require('puppeteer')
 
 describe('Extrayendo informacion', () =>{
 
-    it('Extraer el titulo del elemento', async() => {
-
-        const browser = await puppeteer.launch({
+    let browser
+    let page
+    
+    beforeEach( async() => {
+        browser = await puppeteer.launch({
             headless: false,
             slowMo: 0,
             defaultViewport: null
 
         })
-        const page = await browser.newPage()
+        page = await browser.newPage()
+
+    })
+
+    afterEach( async() => {
+        await browser.close()
+    })
+
+
+    it('Extraer el titulo del elemento', async() => {
+
         await page.goto('https://platzi.com', {waitUntil: 'networkidle0' })
         
         const titulo = await page.title()
@@ -19,19 +31,11 @@ describe('Extrayendo informacion', () =>{
         console.log('Titulo: ', titulo)
         console.log('Url: ', url)
 
-        await browser.close()
-
     }, 500000 )
 
     it('Extraer la informacion de un elemento',async() => {
 
-        const browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 0,
-            defaultViewport: null
-
-        })
-        const page = await browser.newPage()
+       
         await page.goto('https://platzi.com', {waitUntil: 'networkidle0' })
         await page.waitForSelector('#Header-v2 > nav.Nav-header.Nav-header-desktopCtas > div.Menu > div > div > ul > li:nth-child(7) > a')
         
@@ -44,9 +48,6 @@ describe('Extrayendo informacion', () =>{
         const texto = await propiedad.jsonValue()
 
         console.log('texto: ', texto)
-
-        
-        await browser.close()
 
     }, 500000 )
 
