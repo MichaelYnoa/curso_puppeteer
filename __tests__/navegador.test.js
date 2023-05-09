c2e6374edd6ecb34860630b06b0456b9f5dc001f
MichaelYnoa <michaelaraujoynoa@gmail.com>
2023-05-09 10:27:58 -0400

    open new page and move around in the page

--- __tests__/navegador.test.js
+++ __tests__/navegador.test.js
@@ -7,17 +7,36 @@ describe(' Mi primer test en puppeteer', () =>{
         const browser = await puppeteer.launch({
             headless: false,
             slowMo: 0,
-            devtools: false,
-        //    defaultViewport: {
-        //        width: 2100,
-        //        height: 1080
-        //    }
+            defaultViewport: null
+
         })
         const page = await browser.newPage()
-        await page.goto('https://automationexercise.com/')
+        await page.goto('https://github.com/')
         await page.waitForTimeout (5000)
+        await page.waitForSelector('img')
+
+        await page.reload()
+        await page.waitForTimeout (5000)
+        await page.waitForSelector('img')
+
+        await page.goto('https://platzi.com/')
+        await page.waitForSelector('#Header-v2 > nav.Nav-header.Nav-header-desktopCtas > div.Logo > div > a > div > figure > img')
+        //await page.waitForXPath("(//img[@alt='Platzi Logo'])[2]")
+
+        await page.goBack()
+        await page.goForward()
+
+        //abrir otra pagina
+        const page2 = await browser.newPage()
+        await page2.goto('https://google.com/')
+
+
         await browser.close()
 
-    }, 50000 )
+
+
+
+
+    }, 500000 )
 
 })
